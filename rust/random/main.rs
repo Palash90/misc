e8f38cb1fba4f3ecb38ed63ccb71e7e20f863200
Palash <palash.kanti.kundu.90@gmail.com>
2022-10-02 22:42:16 +0530

    random

---
+++ rust/random/main.rs
@@ -0,0 +1,31 @@
+fn main(){
+    let s = String::from("Hi");
+
+    let mut s=random(s);
+    change_string(&mut s);
+
+    print(&s);
+    
+    // Why the following is possible?
+
+    let d=&s;    
+    let f = &s;
+
+    print(f);
+    print(d);
+  //  change_string(d);
+}
+
+fn random(mut s: String)->String{
+    s.push_str("rando");
+    print(&s);
+    s
+}
+
+fn change_string(s : &mut String) {
+    s.push_str("Hello");
+}
+
+fn print(s: &String){
+    println!("{s}");
+}
