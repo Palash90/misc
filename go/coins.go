5aca3894e86b2db0456599288c7b6cfac333c336
Palash <palash.kanti.kundu.90@gmail.com>
2022-11-10 14:04:16 +0530

    coins dynamic

--- go/coins.go
+++ go/coins.go
@@ -6,15 +6,24 @@ func getNumOfCoins(n int) int{
     num := 0
     //    fmt.Println()
     for n>0 {
-        if n >= 500{
+        if n >= 2000{
+            //      fmt.Println("2000")
+            n = n - 2000
+        } else if n >= 500{
             //            fmt.Println("500")
             n = n - 500
-        } else if n >= 100{
+        } else if n >= 200{
+            //            fmt.Println("200")
+            n = n - 200
+        }else if n >= 100{
             //            fmt.Println("100")
             n = n - 100
         } else if n >= 50{
             //            fmt.Println("50")
             n = n - 50
+        } else if n >= 20{
+            //            fmt.Println("20")
+            n = n - 20
         } else if n >= 10 {
             //            fmt.Println("10")
             n = n - 10
@@ -35,24 +44,14 @@ func getNumOfCoins(n int) int{
     return num
 }
 
-func coinsCalc(n int) int {
-    num500 := n/500
-    num100 := (n%500)/100
-    num50 := (n%100)/50
-    num10 := (n%50)/10
-    num5 := (n%10)/5
-    num2 := (n%5)/2
-    num1 := (n%5)%2
-    num :=  num500 + num100 + num50 + num10 + num5 + num2 + num1
-
-    //    fmt.Println("500", num500)
-    //   fmt.Println("100", num100)
-    // fmt.Println("50", num50)
-    // fmt.Println("10", num10)
-    // fmt.Println("5", num5)
-    // fmt.Println("2", num2)
-    // fmt.Println("1", num1)
-    return num;
+func coinsCalc(n int, denominations []int) int {
+    numOfCoins := 0
+    money := n
+    for _, denomination := range denominations {
+        numOfCoins += money/denomination
+        money = money%denomination
+    }
+    return numOfCoins;
 }
 
 func main(){
@@ -60,13 +59,14 @@ func main(){
     fmt.Scan(&n)
     for i:=0; i<n; i++{
 
-        coins := getNumOfCoins(i)
-        coins_c := coinsCalc(i)
+        //        coins := getNumOfCoins(i)
+        coinsCalc(i, []int{2000,500,200,100,50,20,10,5, 2,1})
 
-        if coins != coins_c{
-            fmt.Println("Broke for", i)
-            break;
-        }
+        //        if coins != coins_c{
+        //            fmt.Println("Broke for", i)
+        //            break;
+        //        }
+        //        fmt.Println(i, coins_c)
     }
     fmt.Println("New Algorithm Works")
 }
