cd56f1a83babbc053d666d25a24a390259153151
Palash <palash.kanti.kundu.90@gmail.com>
2022-11-16 16:38:58 +0530

    Tower of hanoi with move count

--- algorithms/ex/tower_of_hanoi.js
+++ algorithms/ex/tower_of_hanoi.js
@@ -1,12 +1,22 @@
+var moveCounter = 0;
 function move(n, source, auxiliary, dest) {
+    if(n === 0){
+        return
+    }
     if(n === 1){
         console.log("Moving", n, "from", source, "to", dest)
+        moveCounter++
         return
     }
     move(n-1, source, dest, auxiliary);
     console.log("Moving", n, "from", source, "to", dest)
+    moveCounter++
     move(n-1, auxiliary, source, dest)
 
 }
 
-move(4, 'S', 'A', 'D')
+for(var i = 0; i <= 10101010101010101010; i++){
+    moveCounter = 0;
+    move(i, 'S', 'A', 'D')
+    console.log("Number of disks", i, "Total move", moveCounter)
+}
