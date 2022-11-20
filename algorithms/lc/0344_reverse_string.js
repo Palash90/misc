21035cfc21efd355e2e095b3e8f1a6f71bd8405e
Palash <palash.kanti.kundu.90@gmail.com>
2022-11-20 11:01:48 +0530

    Reverse String

---
+++ algorithms/lc/0344_reverse_string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for(var i = 0; i<s.length/2; i++){
+        var temp = s[s.length - i - 1]
+        s[s.length-i-1] = s[i]
+        s[i]=temp
+    }
+    return s
+};
+
+console.log(reverseString(["h", "e","l","l","0"]))
+console.log(reverseString(["h", "a","n","n","a", "h"]))
+console.log(reverseString(["p", "a","l","a","s", "h"]))
+
+
+
