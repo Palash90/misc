a6f6a7a01acffa6f489c28371000530bc09a5f7b
Palash <palash.kanti.kundu.90@gmail.com>
2022-11-24 10:54:30 +0530

    bst

---
+++ algorithms/random/random_bst.js
@@ -0,0 +1,74 @@
+class Node {
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var inserted = false;
+        if(this.root === null){
+            this.root = new Node(value)
+            inserted = true
+            return
+        }
+
+        var current = this.root
+        while(!inserted){
+            if(value<current.value){
+                if(current.left === null){
+                    current.left = new Node(value)
+                    inserted = true
+                } else {
+                    current = current.left
+                }
+            }
+
+            if(value > current.value){
+                if(current.right === null){
+                    current.right = new Node(value)
+                    inserted = true
+                } else {
+                    current = current.right
+                }
+            }
+
+        }
+    }
+    lookup(value){
+        //Code here
+    }
+    // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+console.log(JSON.stringify(traverse(tree.root), null, 4))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
+
+
+
+
+
+
