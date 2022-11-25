230cb78c398e6190b58d96893281d3e43f3539a7
Palash <palash.kanti.kundu.90@gmail.com>
2022-11-25 23:41:47 +0530

    Valid Anagram

---
+++ algorithms/lc/0242_valid_anagram.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s1, s2) {
+    if(s1.length !== s2.length) {
+        return false
+    }
+
+    var firstCharMap = buildCharMap(s1)
+    var secondCharMap = buildCharMap(s2)
+
+    var unmatched = false
+
+    var firstCharMapKeys = Object.keys(firstCharMap)
+
+    for(var i = 0; i < firstCharMapKeys.length; i++) {
+        var k = firstCharMapKeys[i]
+        if(firstCharMap[k] !== secondCharMap[k]) {
+            return false
+        } else {
+            delete firstCharMap[k]
+            delete secondCharMap[k]
+        }
+    }
+
+    if(Object.keys(secondCharMap).length > 0){
+        unmatched = true
+    }	
+
+    return !unmatched
+};
+
+const buildCharMap = (s) => {
+    var charMap = {}
+
+    for(var i = 0 ; i < s.length; i++) {
+        if(charMap[s[i]]) {
+            charMap[s[i]]++
+        } else {
+            charMap[s[i]] = 1
+        }
+    }
+
+    return charMap
+}
