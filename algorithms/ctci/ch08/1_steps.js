573c13ed42388cdd31b0171fe2009c4e58f11432
Palash <palash.kanti.kundu.90@gmail.com>
2022-12-03 08:14:09 +0530

    Steps

--- algorithms/ctci/ch08/1_steps.js
+++ algorithms/ctci/ch08/1_steps.js
@@ -18,5 +18,33 @@ const countWays = (n, countsTracker) => {
         return count
     }
 }
-
-console.log(countWaysMemo(35))
+const findStepsMemoized = (n, steps, level, stepSequence, finalSteps) => {
+    var calculatedSteps = {}
+    findSteps(n, steps, level, stepSequence, finalSteps, calculatedSteps)
+}
+const findSteps = (n, steps, level, stepSequence, finalSteps) => {
+    var levelIndentor = ""
+    var indentor = level
+    while(indentor > 0){
+        levelIndentor += "    "
+        indentor--
+    }
+    for(var i = 0; i<steps.length; i++) {
+        var currentStep = steps[i]
+        var remainingSteps = n - currentStep
+        var nsq = [...stepSequence]
+        if(remainingSteps >= 0){
+            nsq.push(currentStep)
+            //            console.log(levelIndentor, currentStep)
+            findSteps(remainingSteps, steps, level + 1, nsq, finalSteps)
+            if(remainingSteps === 0){
+                //                console.log(levelIndentor, nsq)
+                finalSteps.push(nsq)
+            }
+        }
+    }
+}
+var finalSteps = []
+findStepsMemoized(5, [1,2,3,4,5], 0,[], finalSteps)
+console.log(finalSteps)
+console.log("Total Possible Combinations", finalSteps.length)
