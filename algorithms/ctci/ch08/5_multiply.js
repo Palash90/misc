f860235582b19ac33092b21210a6d5bafdcffdea
Palash <palash.kanti.kundu.90@gmail.com>
2022-12-04 17:07:50 +0530

    Multiply

--- algorithms/ctci/ch08/5_multiply.js
+++ algorithms/ctci/ch08/5_multiply.js
@@ -11,10 +11,10 @@ const add = (m, n, r) => {
 for(var i = 0; i < 100; i++) {
     for(var j = 0; j < 100; j++) {
         var result = multiply(i, j)
-        var mult = i * j
+        var mult = Math.floor(i * j)
         if(mult !== result){
             console.log(i, j, multiply(i, j))
         }
-        console.log(result)
+        //        console.log(result)
     }
 }
