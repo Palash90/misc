83fe829e3998487aa1b1f9ca9c44c4271fc9ff71
Palash <palash.kanti.kundu.90@gmail.com>
2023-01-09 21:48:33 +0530

    Completed guessing game without error handling

--- rust/trpl/ch02/guessing_game/src/main.rs
+++ rust/trpl/ch02/guessing_game/src/main.rs
@@ -1,14 +1,30 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Generated guess is {}", secret_number);
 
-    let mut guess = String::new();
+    
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please input a different number");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Low"),
+            Ordering::Greater => println!("Too High"),
+            Ordering::Equal => {
+                 println!("You win");
+                 break;
+            }
+        }
+    }
 }
