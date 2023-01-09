d6bc9e89799948112950d4e90e0b6ef6af98f4c0
Palash <palash.kanti.kundu.90@gmail.com>
2023-01-09 21:58:47 +0530

    Guessing game completed

--- rust/trpl/ch02/guessing_game/src/main.rs
+++ rust/trpl/ch02/guessing_game/src/main.rs
@@ -6,15 +6,16 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Generated guess is {}", secret_number);
-
     
     loop {
         println!("Please input your guess.");
         
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = guess.trim().parse().expect("Please input a different number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+	    Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
     
