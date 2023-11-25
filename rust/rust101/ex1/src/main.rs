5f85825e14d6371c5c8e1c37da3d1e006f2585f0
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2023-11-25 17:33:10 +0530

    Rust 101 Exercise 1

---
+++ rust/rust101/ex1/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    print_first_name("palash");
+    print_last_name("Kundu");
+}
+
+fn print_first_name(first_name: &str){
+    print_console(first_name)
+}
+
+fn print_last_name(last_name: &str) {
+    print_console(last_name)
+}
+
+fn print_console(st: &str) {
+    println!("{st}")
+}
