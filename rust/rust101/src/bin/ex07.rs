aee396c0eaf82c90fa662a475304ec27d2b080de
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2023-11-26 16:33:52 +0530

    Enum exercise

---
+++ rust/rust101/src/bin/ex07.rs
@@ -0,0 +1,26 @@
+// Topic: Use enum to display color in terminal. Use enum as function parameter. 
+
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+fn main() {
+    let my_gemstone_color = Color::Red;
+    display(my_gemstone_color);
+    
+    let my_gemstone_color = Color::Green;
+    display(my_gemstone_color);
+    
+    let my_gemstone_color = Color::Blue;
+    display(my_gemstone_color)
+}
+
+fn display(color: Color) {
+    match color {
+        Color::Red => println!("Ruby"),
+        Color::Green => println!("Emerald"),
+        Color::Blue => println!("Sapphire")
+    }
+}
