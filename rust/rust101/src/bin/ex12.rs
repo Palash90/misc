0518dca4a277cb9b36ef7202d4348645a078c30b
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2023-12-03 14:25:58 +0530

    Impl demo

--- rust/rust101/src/bin/ex12.rs
+++ rust/rust101/src/bin/ex12.rs
@@ -24,7 +24,7 @@ struct Dimension {
 
 impl Dimension {
     fn new(length: f64, width: f64, height: f64) -> Self {
-        Dimension {
+        Self {
             length,
             width,
             height,
@@ -46,7 +46,7 @@ struct ShippingBox {
 
 impl ShippingBox {
     fn new(dimension: Dimension, weight: f64, color: Color) -> Self {
-        ShippingBox {
+        Self {
             dimension,
             weight,
             color,
@@ -55,7 +55,7 @@ impl ShippingBox {
 
     fn print_box(&self) {
         println!(
-            "The box has {}, {} weight, {} color",
+            "The box has {}, {}kg weight, {} color",
             self.dimension.display(),
             self.weight,
             self.color.display()
@@ -65,8 +65,8 @@ impl ShippingBox {
 
 fn main() {
     let red_box_light = ShippingBox::new(Dimension::new(2.0, 2.0, 2.0), 40.0, Color::Red);
-    let green_box_medium = ShippingBox::new(Dimension::new(2.0, 2.0, 2.0), 60.0, Color::Green);
-    let blue_box_heavy = ShippingBox::new(Dimension::new(2.0, 2.0, 2.0), 80.0, Color::Blue);
+    let green_box_medium = ShippingBox::new(Dimension::new(2.0, 6.0, 2.0), 60.0, Color::Green);
+    let blue_box_heavy = ShippingBox::new(Dimension::new(2.0, 8.0, 2.0), 80.0, Color::Blue);
     red_box_light.print_box();
     green_box_medium.print_box();
     blue_box_heavy.print_box();
