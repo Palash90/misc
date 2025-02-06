6fe25aafce7850d0c4b4f55d2c71f000a7922832
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2025-02-06 23:48:39 +0530

    Boxed list

--- test/test_rc/src/main.rs
+++ test/test_rc/src/main.rs
@@ -1,12 +1,42 @@
 use std::cell::RefCell;
 use std::sync::Arc; // Change to std::rc::Rc for single threaded programs
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn add_hello(s: Arc<RefCell<String>>) {
     s.borrow_mut().push_str("Hello");
 }
 
 fn add_world(s: Arc<RefCell<String>>) {
     s.borrow_mut().push_str("World");
+}
+
+fn make_linked_list() {
+    let mut b = Box::new(32);
+
+    println!("{b}");
+
+    *b = 20;
+
+    println!("{b}");
+
+    let mut l = Nil;
+
+    println!("{:?}", l);
+
+    l = Cons(2, Box::new(Nil));
+
+    println!("{:?}", l);
+
+    l = Cons(3, Box::new(l));
+
+    println!("{:?}", l);
 }
 
 fn main() {
@@ -18,5 +48,7 @@ fn main() {
     println!("{}", r.borrow());
 
     add_world(Arc::clone(&r));
-    println!("{}", r.borrow())
+    println!("{}", r.borrow());
+
+    make_linked_list();
 }
