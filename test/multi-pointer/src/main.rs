2fe9ecb8a9b683f864e869beac16a6978be5f533
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2025-02-12 13:37:15 +0530

    Still no multi-client

--- test/multi-pointer/src/main.rs
+++ test/multi-pointer/src/main.rs
@@ -1,89 +1,22 @@
+use std::collections::VecDeque;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-
-fn handle_client(mut stream: TcpStream) {
-    println!("Started handling clinet");
-
-    let mut buffer = [0; 512];
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => {
-                // Connection was closed
-                break;
-            }
-            Ok(n) => {
-                // Echo the message back to the client
-                if let Err(e) = stream.write_all(&buffer[0..n]) {
-                    eprintln!("Failed to send response: {}", e);
-                    break;
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to read from socket: {}", e);
-                break;
-            }
-        }
-    }
-}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
     println!("Server listening on port 7878");
 
-    let req_q = Arc::new(Mutex::new(Vec::<TcpStream>::new()));
+    let req_q = Arc::new(Mutex::new(VecDeque::<TcpStream>::new()));
     let thread_req_q = req_q.clone();
 
-    let req_handle = thread::spawn(move || {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    println!("Connnected");
-
-                    {
-                        let thread_req_q = thread_req_q.lock();
-                        match thread_req_q {
-                            Ok(mut thread_req_q) => {
-                                println!("Acquired lock on Arc");
-                                thread_req_q.push(stream);
-                            }
-                            Err(e) => println!("Error acquiring lock: {:?}", e),
-                        }
-                    }
-                    
-                }
-                Err(e) => {
-                    eprintln!("Failed to accept connection: {}", e);
-                }
-            }
-        }
-    });
+    let req_handle = thread::spawn(move || handle_incoming_requests(listener, thread_req_q));
 
     let processor_req_q = req_q.clone();
 
-    let proc_handle = thread::spawn(move || {
-        loop {
-            {
-                let processor_req_q = processor_req_q.lock().unwrap();
-                println!("Processing thread acquired mutex lock, number of streams to process {}", processor_req_q.len());
-
-                for mut req in processor_req_q.iter() {
-                    let mut r = [0u8; 512];
-                    req.read(&mut r);
-    
-                    println!("{:?}", String::from_utf8_lossy(&r));
-    
-                  //  req.write_all(&r);
-    
-                  //  req.flush();
-                }
-            }
-            println!("Processing thread going to sleep \n");
-            thread::sleep(Duration::from_millis(10));
-        }
-    });
+    let proc_handle = thread::spawn(move || process_request(processor_req_q));
 
     match req_handle.join() {
         Ok(_) => println!("Main thread waiting for request thread to stop"),
@@ -95,4 +28,62 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn process_request(processor_req_q: Arc<Mutex<VecDeque<TcpStream>>>) -> ! {
+    loop {
+        {
+            let mut queue = processor_req_q.lock().unwrap();
+            println!(
+                "Processing thread acquired mutex lock, number of streams to process {}",
+                queue.len()
+            );
+
+            while let Some(mut req) = queue.pop_front() {
+                let mut r = [0u8; 512];
+                match req.read(&mut r) {
+                    Ok(0) => {
+                        // Client disconnected
+                        println!("Client disconnected, removing stream");
+                    }
+                    Ok(n) => {
+                        println!("{:?}", String::from_utf8_lossy(&r[..n]));
+                        req.write_all(&r[..n]).unwrap();
+                        req.flush().unwrap();
+                        queue.push_back(req); // Put back into queue if still active
+                    }
+                    Err(_) => {
+                        // Error means stream is not ready yet, put it back
+                        queue.push_back(req);
+                    }
+                }
+            }
+        }
+        println!("Processing thread going to sleep \n");
+        thread::sleep(Duration::from_millis(10));
+    }
+}
+
+fn handle_incoming_requests(listener: TcpListener, thread_req_q: Arc<Mutex<VecDeque<TcpStream>>>) {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Connnected");
+
+                {
+                    let thread_req_q = thread_req_q.lock();
+                    match thread_req_q {
+                        Ok(mut thread_req_q) => {
+                            println!("Acquired lock on Arc");
+                            thread_req_q.push_back(stream);
+                        }
+                        Err(e) => println!("Error acquiring lock: {:?}", e),
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+            }
+        }
+    }
 }
